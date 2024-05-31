35a9d1b512cc7e25d379ebc82eec03f3cdc93743
Cristian Fernando Dávila López <krif07@gmail.com>
2024-05-31 10:39:22 -0500

    adding users and enconding password

--- src/main/java/com/co/cfd/springboot/app/springbootcrud/services/UserServiceImpl.java
+++ src/main/java/com/co/cfd/springboot/app/springbootcrud/services/UserServiceImpl.java
@@ -1,12 +1,15 @@
 package com.co.cfd.springboot.app.springbootcrud.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.co.cfd.springboot.app.springbootcrud.entities.Role;
 import com.co.cfd.springboot.app.springbootcrud.entities.User;
 import com.co.cfd.springboot.app.springbootcrud.repositories.IRoleRepository;
 import com.co.cfd.springboot.app.springbootcrud.repositories.IUserRepository;
@@ -32,6 +35,18 @@ public class UserServiceImpl implements IUserService {
     @Override
     @Transactional
     public User save(User user) {
+
+        Optional<Role> optionalRoleUser = roleRepository.findByName("ROLE_USER");
+        List<Role> roles = new ArrayList<>();
+
+        optionalRoleUser.ifPresent(role -> roles.add(role));
+        if(user.isAdmin()) {
+            Optional<Role> optionalRoleAdmin = roleRepository.findByName("ROLE_ADMIN");
+            optionalRoleAdmin.ifPresent(roles::add);
+        }
+
+        user.setRoles(roles);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return repository.save(user);
     }
 
