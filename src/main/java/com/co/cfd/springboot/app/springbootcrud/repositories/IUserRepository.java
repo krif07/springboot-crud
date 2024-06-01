4ddf8fcbf1ea8dd7a9ae960d45533b5080137272
Cristian Fernando Dávila López <krif07@gmail.com>
2024-05-31 22:48:19 -0500

    jpa user details service

--- src/main/java/com/co/cfd/springboot/app/springbootcrud/repositories/IUserRepository.java
+++ src/main/java/com/co/cfd/springboot/app/springbootcrud/repositories/IUserRepository.java
@@ -1,9 +1,13 @@
 package com.co.cfd.springboot.app.springbootcrud.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.co.cfd.springboot.app.springbootcrud.entities.User;
 
+
 public interface IUserRepository extends CrudRepository<User, Long> {
     boolean existsByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
