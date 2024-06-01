1c370e25b43f42bdf6b7aee6abf1493a13983adc
Cristian Fernando Dávila López <krif07@gmail.com>
2024-06-01 11:49:00 -0500

    Fixed existdbvalidation and existbyuser vatidation

--- src/main/java/com/co/cfd/springboot/app/springbootcrud/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/co/cfd/springboot/app/springbootcrud/security/filter/JwtAuthenticationFilter.java
@@ -77,7 +77,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         String token = Jwts.builder()
                         .subject(username)
                         .claims(claims)
-                        .expiration(new Date(System.currentTimeMillis() + 36000))
+                        .expiration(new Date(System.currentTimeMillis() + 360000))
                         .issuedAt(new Date())
                         .signWith(SECRET_KEY)
                         .compact();
