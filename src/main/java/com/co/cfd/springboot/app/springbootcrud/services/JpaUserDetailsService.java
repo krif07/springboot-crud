2b8da68a9321e14a0f4097cbfd9afe708fe514b6
Cristian Fernando Dávila López <krif07@gmail.com>
2024-05-31 23:05:24 -0500

    authentication filter - filter to authenticate

--- src/main/java/com/co/cfd/springboot/app/springbootcrud/services/JpaUserDetailsService.java
+++ src/main/java/com/co/cfd/springboot/app/springbootcrud/services/JpaUserDetailsService.java
@@ -33,15 +33,16 @@ public class JpaUserDetailsService implements UserDetailsService {
         List<GrantedAuthority> authorities = user.getRoles()
             .stream().map(role -> new SimpleGrantedAuthority(role.getName()))
             .collect(Collectors.toList());
-            return new org.springframework.security.core.userdetails
-                .User(
-                    user.getUsername(), 
-                    user.getPassword(),
-                    user.isEnabled(), 
-                    true, 
-                    true, 
-                    true, 
-                    authorities);
+        
+        return new org.springframework.security.core.userdetails
+            .User(
+                user.getUsername(), 
+                user.getPassword(),
+                user.isEnabled(), 
+                true, 
+                true, 
+                true, 
+                authorities);
     }
 
 }
